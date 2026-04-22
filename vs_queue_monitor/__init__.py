10ff99e4aad8d9c338e511c9e450063d06223ee9
Shubi Maja <shubimaja@gmail.com>
2026-04-22 23:41:40 +0300

    Align graph button highlights with history controls

--- vs_queue_monitor/__init__.py
+++ vs_queue_monitor/__init__.py
@@ -1,6 +1,6 @@
 """VS Queue Monitor - Vintage Story client log queue monitor."""
 
-VERSION = "1.0.329"
+VERSION = "1.0.330"
 APP_DISPLAY_NAME = "VS Queue Monitor"
 APP_TAGLINE = "Vintage Story client log queue monitor"
 GITHUB_REPO_URL = "https://github.com/ShubiMaja/vs-queue-monitor"
